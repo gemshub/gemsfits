7e32a70ec1d985230d53dd057eb2ab8b73fe14c8
ext_miron_d <ext_miron_d@376c6ad0-4da0-11e1-87d0-276bdbf53620>
2013-05-02 14:41:40 +0000

    Correction to comments
    git-svn-id: svn+ssh://savannah01.psi.ch/repos/gems/GEMSFIT/branches/devDanFit@152 376c6ad0-4da0-11e1-87d0-276bdbf53620

--- csvtoejdb/csvtoejdb/csvtoejdb.pro
+++ csvtoejdb/csvtoejdb/csvtoejdb.pro
@@ -1,67 +1,38 @@
 
 # CASH/ solcash CASH.csv
 
-TEMPLATE = app
-CONFIG += console
-CONFIG -= qt
+TEMPLATE        = app
+CONFIG         += console
+CONFIG         -= qt
 
 
-CONFIG -= qt
-CONFIG += warn_on
-CONFIG += console
-CONFIG += serial release
-
-CONFIG( release,  debug|release ) {
-        message( "Configuring for release build ..." )
-        QMAKE_CFLAGS_RELEASE = -g -pedantic
-        QMAKE_CXXFLAGS_RELEASE = -g -pedantic
-}
-
-CONFIG( debug,  debug|release ) {
-        DEFINES += GEMSFIT_DEBUG
-        QMAKE_CFLAGS_DEBUG = -g
-        QMAKE_CXXFLAGS_DEBUG = -g
-        message( "Configuring for debug build ..." )
-}
-
-CONFIG( mpi, serial|mpi ) {
-        message( "Configuring for OPEN_MPI build ..." )
-        QMAKE_CC	= mpicc
-        QMAKE_CXX	= mpic++
-        QMAKE_CFLAGS += $$system(mpicc --showme:compile) -fopenmp
-        QMAKE_CXXFLAGS += -Wall -pedantic -fexceptions $$system(mpic++ --showme:compile) -Wl,-rpath -Wl,/usr/lib -Wl,-Bsymbolic-functions -fopenmp
-        LIBS += -L/usr/lib/postgresql/9.1/lib/ -L/usr/local/dislin/ $$system(mpic++ --showme:link) -ldislin -lXm -lecpg -lnlopt -lm -lboost_filesystem -lboost_system -llapack -lblas -larmadillo -lpthread -lz -fopenmp -ljansson
-}
-
-#contains( CONFIG, serial ) {
-CONFIG( serial, serial|mpi ) {
-        message( "Configuring for serial build ..." )
-        QMAKE_CC	= gcc
-        QMAKE_CXX	= g++
-        QMAKE_CFLAGS += -fopenmp
-        QMAKE_CXXFLAGS += -Wall -pedantic -fexceptions -Wl,-rpath -Wl,/usr/lib -Wl,-Bsymbolic-functions -fopenmp
-        LIBS += -L/usr/lib/postgresql/9.1/lib/ -L/usr/local/dislin/ -ldislin -lXm -lecpg -lnlopt -lm -lboost_filesystem -lboost_system -llapack -lblas -larmadillo -lpthread -lz -fopenmp -ljansson
-}
+CONFIG         -= qt
+CONFIG         += warn_on
+CONFIG         += console
+CONFIG         += serial release
 
 
-QMAKE_CXXFLAGS += -std=gnu99
-QMAKE_CFLAGS += -std=gnu99
+QMAKE_CC	= gcc
+QMAKE_CXX	= g++
+QMAKE_CXXFLAGS += -std=gnu99 -Wall -pedantic -fexceptions -Wl,-rpath -Wl,/usr/lib -Wl,-Bsymbolic-functions -fopenmp
+QMAKE_CFLAGS   += -std=gnu99 -fopenmp
+LIBS           += -lboost_filesystem -lboost_system -lz -fopenmp -ljansson
 
 
-EJDB_CPP   =  ../../gemsfit2/tcejdb
-MAIN_CPP      =  ./main
+EJDB_CPP        =  ../../gemsfit2/tcejdb
+MAIN_CPP        =  ./main
 
-EJDB_H   =  $$EJDB_CPP
-MAIN_H        =   $$MAIN_CPP
+EJDB_H          =  $$EJDB_CPP
+MAIN_H          =   $$MAIN_CPP
 
 
-DEPENDPATH   += $$EJDB_H
-DEPENDPATH   += $$MAIN_H
+DEPENDPATH     += $$EJDB_H
+DEPENDPATH     += $$MAIN_H
 
-INCLUDEPATH   += $$EJDB_H
-INCLUDEPATH   += $$MAIN_H
+INCLUDEPATH    += $$EJDB_H
+INCLUDEPATH    += $$MAIN_H
 
-OBJECTS_DIR       = obj
+OBJECTS_DIR     = obj
 
 include($$EJDB_CPP/tcejdb.pri)
 include($$MAIN_CPP/main.pri)
--- csvtoejdb/csvtoejdb/main/main.cpp
+++ csvtoejdb/csvtoejdb/main/main.cpp
@@ -7,7 +7,6 @@
 #include <boost/lexical_cast.hpp>
 
 using namespace std;
-//using namespace keys;
 
 static EJDB *jb;
 
@@ -73,12 +72,28 @@ int main(int argc, char *argv[])
         bson exp;
         bson_init(&exp);
 
+        // cheking if the combination of sample name and dataset name is unique in the datasbase
+        bson bq1;
+        bson_init_as_query(&bq1);
+
         //first level objects: sample, expdataset, sT, sP.
         for (unsigned int i=0; i<headline.size(); ++i)
         {
             if ((headline[i]==expsample) || (headline[i]==expdataset) || (headline[i]==Tunit) || (headline[i]==Punit)|| (headline[i]==Vunit) )
             {
                 bson_append_string(&exp, headline[i].c_str(), row[i].c_str());
+                if (headline[i]==expdataset)
+                {
+                    bson_append_start_object(&bq1, expdataset);
+                    bson_append_string(&bq1, "$begin", row[i].c_str());
+                    bson_append_finish_object(&bq1);
+                }
+                if (headline[i]==expsample)
+                {
+                    bson_append_start_object(&bq1, expsample);
+                    bson_append_string(&bq1, "$begin", row[i].c_str());
+                    bson_append_finish_object(&bq1);
+                }
             }
             else if  ((headline[i]==sT) || (headline[i]==sP))
             {
@@ -90,6 +105,30 @@ int main(int argc, char *argv[])
             }
 
          }
+
+        bson_finish(&bq1);
+        EJQ *q1 = ejdbcreatequery(jb, &bq1, NULL, 0, NULL);
+        uint32_t count;
+        TCLIST *res = ejdbqryexecute(coll, q1, &count, 0, NULL);
+
+        if (count > 0)
+        {
+            cout << "The combination sample name and expdataset in the input csv file is already present in the database" << endl;
+            //Now print the result set records
+             for (int i = 0; i < TCLISTNUM(res); ++i) {
+                 void *bsdata = TCLISTVALPTR(res, i);
+                 char *bsdata_ = static_cast<char*>(bsdata);
+                 bson_print_raw(stderr, bsdata_, 0);
+             }
+             fprintf(stderr, "\n");
+             exit(1);
+        }
+
+        //Dispose result set
+        tclistdel(res);
+
+        //Dispose query
+        ejdbquerydel(q1);
 
         // 2nd level - bulk composition of chemical system for this experiment
         // array of components
@@ -128,7 +167,6 @@ int main(int argc, char *argv[])
                     bson_append_string(&exp, Qunit, row[i].c_str());
                 }
                 bson_append_finish_object(&exp);
-
             }
         }
         //++ END array sbcomp ++//
@@ -168,13 +206,13 @@ int main(int argc, char *argv[])
 
                     if (!h_phases) // START if h_phases
                     {
-                        bson_append_start_object(&exp, boost::lexical_cast<string>(phc).c_str());
+                        bson_append_start_object(&exp, boost::lexical_cast<string>(phc).c_str()); // START phase object
                         phc++;
                         bson_append_string(&exp, phase, phase_name.c_str());
                         phases.push_back(phase_name);
                         ic = 0;
 
-                        // START check if there is phprop & phcomp data in the CSV
+                        // START check if there is phprop & phcomp data exists in the CSV
                         for (unsigned int j=0; j<headline.size(); ++j)
                         {
                             if ((strncmp(headline[j].c_str(),phase, 5) == 0) && (!row[j].empty()))
@@ -187,12 +225,12 @@ int main(int argc, char *argv[])
                                     // getting the name of the property phase e.g. Si from phase.aq_gen.Si, or pQunt from pahse.aq_gen.pQunt
                                     ph_prop = headline[j].substr((pos_end+f1.length()),(headline[j].size()));
 
-                                    // qunatity of this pahse in the experiment
+                                    // if property present
                                     if (((ph_prop == pQnt) || (ph_prop == pH) || (ph_prop == pV) ||  (ph_prop == Eh) || (ph_prop == IS) || (ph_prop == all) ||  (ph_prop == sArea)) && (!row[j].empty()))
                                     {
                                         h_phprop = true;
                                     }
-
+                                    // if composition present
                                     if (((ph_prop != pQnt) && (ph_prop != pH) && (ph_prop != pV) &&  (ph_prop != Eh) && (ph_prop != IS) && (ph_prop != all) &&  (ph_prop != sArea)) && (strncmp(ph_prop.c_str(),"s", 1) != 0) && (!row[j].empty()))
                                     {
                                         h_phcomp = true;
@@ -205,7 +243,6 @@ int main(int argc, char *argv[])
                         if (h_phprop)
                         {
                             bson_append_start_array(&exp, phprop);
-
                             // get phase poperties
                             for (unsigned int j=0; j<headline.size(); ++j)
                             {
@@ -213,27 +250,27 @@ int main(int argc, char *argv[])
                                 {
                                     pos_start = headline[j].find(f1);
                                     pos_end   = headline[j].find(f1,pos_start+1);
-                                    // getting the phase properties and composition
+                                    // getting the phase properties
                                     if (phase_name == headline[j].substr((pos_start+f1.length()),(pos_end-pos_start-f1.length())))
                                     {
-                                        // getting the name of the property phase e.g. Si from phase.aq_gen.Si, or pQunt from pahse.aq_gen.pQunt
+                                        // getting the name of the property phase e.g. Si from phase.aq_gen.Si
                                         ph_prop = headline[j].substr((pos_end+f1.length()),(headline[j].size()));
 
-                                        // qunatity of this pahse in the experiment
+                                        // amount of the property of the phase pahse in the experiment
                                         if (((ph_prop == pQnt) || (ph_prop == pH) || (ph_prop == pV) ||  (ph_prop == Eh) || (ph_prop == IS) || (ph_prop == all) ||  (ph_prop == sArea)) && (!row[j].empty()))
                                         {
-                                            bson_append_start_object(&exp, boost::lexical_cast<string>(ic).c_str());
+                                            bson_append_start_object(&exp, boost::lexical_cast<string>(ic).c_str()); // START property object
                                             ic++;
-
                                             bson_append_string(&exp, property, ph_prop.c_str());
                                             bson_append_double(&exp, pQnt, atof(row[j].c_str()));
+
                                             // checking if there are errors and units included in the CSV and adding tem in the database
                                             if ((headline[j+1]==_error))
                                             {
                                                 ++j;
                                                 if ((!row[j].empty()))
                                                 {
-                                                bson_append_double(&exp, Qerror, atof(row[j].c_str()));
+                                                    bson_append_double(&exp, Qerror, atof(row[j].c_str()));
                                                 }
                                             }
                                             if ((headline[j+1]==_unit) && (!row[j+1].empty()))
@@ -241,8 +278,7 @@ int main(int argc, char *argv[])
                                                 ++j;
                                                 bson_append_string(&exp, Qunit, row[j].c_str());
                                             }
-
-                                            bson_append_finish_object(&exp);
+                                            bson_append_finish_object(&exp); // END property object
                                             ph_old = phase_name;
                                         }
                                     }
@@ -262,40 +298,40 @@ int main(int argc, char *argv[])
                             {
                                 if ((strncmp(headline[j].c_str(),phase, 5) == 0) && (!row[j].empty()))
                                 {
-                                pos_start = headline[j].find(f1);
-                                pos_end   = headline[j].find(f1,pos_start+1);
-                                // getting the phase_name properties and composition from CSV
-                                if (phase_name == headline[j].substr((pos_start+f1.length()),(pos_end-pos_start-f1.length())))
-                                {
-                                    // getting the name of the property phase e.g. Si from phase.aq_gen.Si, or pQunt from pahse.aq_gen.pQunt
-                                    ph_prop = headline[j].substr((pos_end+f1.length()),(headline[j].size()));
-
-                                    // qunatity of this pahse in the experiment
-                                    if (((ph_prop != pQnt) && (ph_prop != pH) && (ph_prop != pV) &&  (ph_prop != Eh) && (ph_prop != IS) && (ph_prop != all) &&  (ph_prop != sArea)) && (strncmp(ph_prop.c_str(),"s", 1) != 0) && (!row[j].empty()))
+                                    pos_start = headline[j].find(f1);
+                                    pos_end   = headline[j].find(f1,pos_start+1);
+                                    // getting the phase_name properties and composition from CSV
+                                    if (phase_name == headline[j].substr((pos_start+f1.length()),(pos_end-pos_start-f1.length())))
                                     {
-                                        bson_append_start_object(&exp, boost::lexical_cast<string>(ic).c_str());
-                                        ic++;
+                                        // getting the name of the property phase e.g. Si from phase.aq_gen.Si, or pQunt from pahse.aq_gen.pQunt
+                                        ph_prop = headline[j].substr((pos_end+f1.length()),(headline[j].size()));
 
-                                        bson_append_string(&exp, element, ph_prop.c_str());
-                                        bson_append_double(&exp, eQnt, atof(row[j].c_str()));
-                                        // checking if there are errors and units included in the CSV and adding tem in the database
-                                        if ((headline[j+1]==_error))
+                                        // qunatity of this pahse in the experiment
+                                        if (((ph_prop != pQnt) && (ph_prop != pH) && (ph_prop != pV) &&  (ph_prop != Eh) && (ph_prop != IS) && (ph_prop != all) &&  (ph_prop != sArea)) && (strncmp(ph_prop.c_str(),"s", 1) != 0) && (!row[j].empty()))
                                         {
-                                            ++j;
-                                            if ((!row[j].empty()))
+                                            bson_append_start_object(&exp, boost::lexical_cast<string>(ic).c_str()); // START phase element object
+                                            ic++;
+                                            bson_append_string(&exp, element, ph_prop.c_str());
+                                            bson_append_double(&exp, eQnt, atof(row[j].c_str()));
+
+                                            // checking if there are errors and units included in the CSV and adding tem in the database
+                                            if ((headline[j+1]==_error))
                                             {
-                                            bson_append_double(&exp, Qerror, atof(row[j].c_str()));
+                                                ++j;
+                                                if ((!row[j].empty()))
+                                                {
+                                                bson_append_double(&exp, Qerror, atof(row[j].c_str()));
+                                                }
                                             }
+                                            if ((headline[j+1]==_unit) && (!row[j+1].empty()))
+                                            {
+                                                ++j;
+                                                bson_append_string(&exp, Qunit, row[j].c_str());
+                                            }
+                                            bson_append_finish_object(&exp); // END phase element object
+                                            ph_old = phase_name;
                                         }
-                                        if ((headline[j+1]==_unit) && (!row[j+1].empty()))
-                                        {
-                                            ++j;
-                                            bson_append_string(&exp, Qunit, row[j].c_str());
-                                        }
-                                        bson_append_finish_object(&exp);
-                                        ph_old = phase_name;
                                     }
-                                }
                                 }
                             }
                             //++ END array phcomp ++//
@@ -317,15 +353,12 @@ int main(int argc, char *argv[])
                                     if (phase_name == headline[j].substr((pos_start+f1.length()),(pos_end-pos_start-f1.length())))
                                     {
                                         ph_prop = headline[j].substr((pos_end+f1.length()),(headline[j].size()));
-
-                                        // specie
+                                        // species
                                         if ((strncmp(ph_prop.c_str(),"s", 1) == 0) && (!row[j].empty()))
                                         {
-                                            bson_append_start_object(&exp, boost::lexical_cast<string>(ic).c_str());
+                                            bson_append_start_object(&exp, boost::lexical_cast<string>(ic).c_str()); // START species object
                                             ic++;
-
                                             ph_prop = ph_prop.substr(1,ph_prop.length()); // deleting the "s"
-
                                             bson_append_string(&exp, species, ph_prop.c_str());
                                             bson_append_double(&exp, sQnt, atof(row[j].c_str()));
 
@@ -344,9 +377,8 @@ int main(int argc, char *argv[])
                                                 bson_append_string(&exp, Qunit, row[j].c_str());
                                             }
 
-                                            bson_append_finish_object(&exp);
+                                            bson_append_finish_object(&exp); // END species object
                                             ph_old = phase_name;
-
                                         }
                                     }
                                 }
@@ -355,7 +387,7 @@ int main(int argc, char *argv[])
                             bson_append_finish_array(&exp);
                         }
                         // end object in the array phases
-                        bson_append_finish_object(&exp);
+                        bson_append_finish_object(&exp); // END phase object
                     } // END if h_phases
 
                 } // END ph_new != ph_old
@@ -369,9 +401,8 @@ int main(int argc, char *argv[])
         phases.clear(); // clearing the vector that holds the pahses in one experiment
         h_phases = false; // reseting the phases handle that cheks if one phase key was not present before
 
-
         bson_finish(&exp);
-        ejdbsavebson(coll, &exp, &oid);
+        ejdbsavebson(coll, &exp, &oid); // saving the document in the database
         bson_destroy(&exp);
     } // END getting data from CSV
 
@@ -379,13 +410,13 @@ int main(int argc, char *argv[])
 
     //Now select records.
     //Query: {'expdataset' : {'$begin' : 'CH'}} //Expdataset name starts with 'CH' string
-    bson bq1;
-    bson_init_as_query(&bq1);
-    bson_append_start_object(&bq1, expdataset);
-    bson_append_string(&bq1, "$begin", "CH");
-    bson_append_finish_object(&bq1);
-    bson_finish(&bq1);
-    EJQ *q1 = ejdbcreatequery(jb, &bq1, NULL, 0, NULL);
+//    bson bq1;
+//    bson_init_as_query(&bq1);
+//    bson_append_start_object(&bq1, expdataset);
+//    bson_append_string(&bq1, "$begin", "CH");
+//    bson_append_finish_object(&bq1);
+//    bson_finish(&bq1);
+//    EJQ *q1 = ejdbcreatequery(jb, &bq1, NULL, 0, NULL);
 
 //    {   "sample" : "",
 //        "expdataset" : ["CHO4", "CH04D"],
@@ -409,24 +440,16 @@ int main(int argc, char *argv[])
     bson_append_finish_array(&bq2);
     bson_append_finish_object(&bq2);
 
-//    bson_append_start_array(&bq2, sT);
-//    bson_append_string(&bq2, "$bt", "10");
-//    bson_append_string(&bq2, "$bt", "1000");
-//    bson_append_finish_array(&bq2);
-//    bson_append_finish_object(&bq2);
-
     bson_finish(&bq2);
     EJQ *q2 = ejdbcreatequery(jb, &bq2, NULL, 0, NULL);
 
-
     uint32_t count;
-    TCLIST *res = ejdbqryexecute(coll, q1, &count, 0, NULL);
     TCLIST *res2 = ejdbqryexecute(coll, q2, &count, 0, NULL);
     fprintf(stderr, "\n\nRecords found: %d\n", count);
 
     //Now print the result set records
-     for (int i = 0; i < TCLISTNUM(res); ++i) {
-         void *bsdata = TCLISTVALPTR(res, i);
+     for (int i = 0; i < TCLISTNUM(res2); ++i) {
+         void *bsdata = TCLISTVALPTR(res2, i);
          char *bsdata_ = static_cast<char*>(bsdata);
          bson_print_raw(stderr, bsdata_, 0);
      }
@@ -443,13 +466,12 @@ int main(int argc, char *argv[])
 //     fprintf(stderr, "\n");
 
     //Dispose result set
-    tclistdel(res);
-
+//    tclistdel(res);
     tclistdel(res2);
 
     //Dispose query
-    ejdbquerydel(q1);
-    bson_destroy(&bq1);
+//    ejdbquerydel(q1);
+//    bson_destroy(&bq1);
 
     ejdbquerydel(q2);
     bson_destroy(&bq2);
