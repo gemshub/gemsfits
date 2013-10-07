553271c1a3761e7a90634a66a7cc9ac1b6810550
kulik <kulik@376c6ad0-4da0-11e1-87d0-276bdbf53620>
2013-10-07 11:03:36 +0000

    Re-arranged documentation files for GEMSFIT2 and csvtoejdb to produce html and doxygen documentation; removed obsolete folders; edited some text documents.
    git-svn-id: svn+ssh://savannah01.psi.ch/repos/gems/GEMSFIT/branches/devDanFit@225 376c6ad0-4da0-11e1-87d0-276bdbf53620

--- docs/load_NaCl_ambient_from_csv.sql
+++
@@ -1,8 +0,0 @@
-﻿COPY (id,temperature,pressure,molality_1,molality_2,code,val,other_1,other_2,other_3,
- other_4,error,species_1_1,species_1_2,species_1_3,species_1_4,
- species_2_1,species_2_2,species_2_3,species_2_4,
- stoic_1_1,stoic_1_2,stoic_1_3,stoic_1_4,stoic_2_1,stoic_2_2,stoic_2_3,stoic_2_4,
- "Reference",ende)
-FROM '/home/dmiron/nacl_298k.csv'
-WITH DELIMITER ';'
-CSV HEADER
