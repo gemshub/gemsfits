17568a8481c9d663b3fffa8e187ce2ffab59e169
svetad <sv.dmytr@gmail.com>
2023-12-12 20:57:40 +0200

    GEMS-218: Fixed CI build for Win64-VS try without GUI

--- cmake/modules/gemsfitsFindDeps.cmake
+++ cmake/modules/gemsfitsFindDeps.cmake
@@ -1,15 +1,3 @@
-
-if(USE_SPDLOG_PRECOMPILED)
-  if(NOT TARGET spdlog::spdlog)
-     find_package(spdlog CONFIG REQUIRED)
-     if(NOT spdlog_FOUND)
-         message(FATAL_ERROR "spdlog library not found")
-     else()
-         message(STATUS "Found spdlog v${spdlog_VERSION}")
-     endif()
-  endif()
-endif()
-
 find_library(NLOPT_LIB nlopt)
 if(NOT NLOPT_LIB)
    message(FATAL_ERROR "nlopt library not found")
