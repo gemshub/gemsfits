1bfca382d117a1d0ae282f7d9692bb3190526c5d
svetad <sv.dmytr@gmail.com>
2024-01-22 15:36:47 +0200

    Fixed ejdb2 install Windows2

--- ci/pipelines/install-VS.bat
+++ ci/pipelines/install-VS.bat
@@ -2,7 +2,6 @@ call activate gemsfits
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-conda list
 echo "Configuring..."
 cmake -G"Visual Studio 16 2019" -A x64 -S . -B build
 echo "Building..."
